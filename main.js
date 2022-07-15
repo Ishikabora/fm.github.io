1d55c0288519f531eb6779e45a258c5acf388564
Ishikabora <96299273+Ishikabora@users.noreply.github.com>
2022-07-15 16:24:55 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,47 @@
+noseX = 0;
+noseY = 0;
+difference = 0;
+leftWristX = 0;
+RightWristx = 0;
+
+function setup()
+{
+   video = createCapture(VIDEO);
+   video.size(550, 500);
+   canvas = createCanvas(550, 550);
+   canvas.position(560, 150);
+   poseNet = ml5.poseNet(video, modelLoaded);
+   poseNet.on('pose', gotPoses);
+}
+
+function draw()
+{
+    background("#969A97");
+    document.getElementById("text_side").innerHTML = "Width and heigth of square will be = "+difference+" px";
+    fill("#f90093");
+    text('Ishika', noseX, noseY);
+    textSize(difference);
+    
+    
+}
+
+function modelLoaded()
+{
+    console.log("PoseNet is Initialized!")
+}
+
+function gotPoses(results)
+{
+    if (results.length > 0)
+    {
+        console.log(results);
+        noseX = results[0].pose.nose.x;
+        noseY = results[0].pose.nose.y;
+        console.log("noseX = "+noseX+"noseY = "+noseY);
+
+        leftWristX = results[0].pose.leftWrist.x;
+        rightWristX = results[0].pose.rightWrist.x;
+        difference = floor(leftWristX - rightWristX);
+        console.log("LeftWristX = "+leftWristX+"RightWristX = "+rightWristX+"difference = "+difference);
+    }
+}
